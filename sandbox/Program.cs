f00369945c3ee73dca91776c27e806fa31c8795f
Gail Fike <Gail Fike@DESKTOP-MQADQLD>
2017-11-10 12:07:07 -0600

    Add project files.

---
+++ sandbox/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
