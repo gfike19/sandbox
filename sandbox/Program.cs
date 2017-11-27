bb7b8bab5c9a7560e7e12f1fb05e79eee75294b3
Gail Fike <Gail Fike@DESKTOP-MQADQLD>
2017-11-27 09:45:51 -0600

    possible working solution, does jump ahead though and so i needs one taken off

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -12,20 +12,24 @@ namespace sandbox
         static void Main(string[] args)
         {
             int i = 0;
-            int user_count = 0;
-
-            while (user_count < 20 && user_count == 0)
+            int user_input = 0;
+            do
             {
-                Console.WriteLine(i);
-                Thread.Sleep(1000);
+                while (!Console.KeyAvailable)
+                {
+                    // Do something
+                    Console.WriteLine(i);
+                    Thread.Sleep(1000);
+                    i++;
+
+                }
                 
                 if (Console.ReadKey(true).Key == ConsoleKey.Spacebar)
                 {
-                    user_count += i;
-                    Console.WriteLine(i + " has been added!");
+                    user_input += i;
+                    Console.WriteLine("The user took " + (i - 1));
                 }
-                i++;
-            }
+            } while (i < 100);
         }
     }
 }
