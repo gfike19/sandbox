ad80ed6fbb81a564cdda9d2ff2be578575116af3
Gail Fike <gfike19@gmail.com>
2018-07-26 13:12:03 -0500

    first commit

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,17 @@ namespace sandbox
     {
         static void Main(string[] args)
         {
-            
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            var byteArray = new byte[4];
+            provider.GetBytes(byteArray);
 
+            int randomInteger = (int)BitConverter.ToUInt32(byteArray, 0) % 9;
+            List<int> listOfLists = new List<int>();
+
+            for(int i = 0; i < randomInteger; i++)
+            {
+
+            }
         }
     }
 }
