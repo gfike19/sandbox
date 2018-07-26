ec5f21aa69cc2b8da9b8ca7e38c7076226f37b73
Gail Fike <gfike19@gmail.com>
2018-07-26 14:05:20 -0500

    got desired result via loops

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -11,17 +11,60 @@ namespace sandbox
     {
         static void Main(string[] args)
         {
+            List<List<int>> listOfLists = new List<List<int>>();
+
+            //create list of lists that has random nunbers
+            while (listOfLists.Count < 5)
+            {
+                List<int> temp = new List<int>();
+
+                for (int j = 0; j < 10; j++)
+                {
+                    int rnum = getRandomNum();
+                    temp.Add(rnum);
+                }
+                listOfLists.Add(temp);
+            }
+
+            //display original list of lists
+            foreach(List<int> each in listOfLists)
+            {
+                Console.Write("[");
+                foreach(int num in each)
+                {
+                    Console.Write(num + ", ");
+                }
+                Console.Write("]\n\n");
+            }
+
+            List<int> collated = new List<int>();
+
+            for(int i = 0; i < 10; i ++)
+            {
+                foreach(List<int> lst in listOfLists)
+                {
+                    collated.Add(lst[i]);
+                }
+            }
+
+            Console.Write("\n\n");
+            foreach(int num in collated)
+            {
+                Console.Write(num + ", ");
+            }
+
+            Console.ReadLine();
+        }
+
+
+        public static int getRandomNum()
+        {
             RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
             var byteArray = new byte[4];
             provider.GetBytes(byteArray);
 
-            int randomInteger = (int)BitConverter.ToUInt32(byteArray, 0) % 9;
-            List<int> listOfLists = new List<int>();
-
-            for(int i = 0; i < randomInteger; i++)
-            {
-
-            }
+            int randomInteger = (int)BitConverter.ToUInt32(byteArray, 0) % 10;
+            return randomInteger;
         }
     }
 }
