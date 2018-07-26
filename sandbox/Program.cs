021e6dd0d6ef8a78c893407c755d656723806db7
Gail Fike <gfike19@gmail.com>
2018-07-26 12:16:08 -0500

    tested sort method of numbers in list

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,38 @@ namespace sandbox
         {
             
 
+            List<int> test = new List<int>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                int rnum = getRandomNum();
+                test.Add(rnum);
+            }
+
+            foreach(int i in test)
+            {
+                Console.Write(i + ", ");
+            }
+
+            test.Sort();
+            Console.WriteLine("\n");
+
+            foreach (int i in test)
+            {
+                Console.Write(i + ", ");
+            }
+
+            Console.ReadLine();
+        }
+        //true random numbers
+        public static int getRandomNum ()
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            var byteArray = new byte[4];
+            provider.GetBytes(byteArray);
+
+            int randomInteger = (int)BitConverter.ToUInt32(byteArray, 0) % 10;
+            return randomInteger;
         }
     }
 }
