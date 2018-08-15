0031b38e5b3842b349ee19e010dbe149b1eb1bd0
Gail Fike <gfike19@gmail.com>
2018-08-15 00:15:30 -0500

    found what I was looking for

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -11,6 +13,32 @@ namespace sandbox
     {
         static void Main(string[] args)
         {
+            List<string> alist = new List<string>
+            {
+                "tyger", "tyger", "burning", "bright"
+            };
+
+            //doesn't work
+            //List<string> newList = alist.RemoveAll();
+            //neither does .Clear()
+            // or this
+            //List<string> newList = new List<string>();
+            //    alist.CopyTo();
+
+            //the following works
+            List<string> newList = alist.Take(alist.Count).ToList();
+
+            foreach (string str in alist)
+            {
+                Console.WriteLine(str);
+            }
+            //may work with mods
+            //alist.foreach (var item in alist)
+            //{
+            //    newList.Add(item);
+            //};
+
+            Console.ReadLine();
         }
     }
 }
