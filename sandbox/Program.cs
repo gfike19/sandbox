2f4f61d9bdfeff1b60dafadca6b4731bae622886
Gail <gfike19@gmail.com>
2019-12-03 14:57:02 -0600

    tried adding random numbers, getting the same one over and over

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,30 +14,43 @@ namespace sandbox
         static void Main(string[] args)
         {
             ConsoleKeyInfo cki;
-            int i = 0;
+            //int i = 0;
             int user_input = 0;
-            int off_count = 0;
+            //int off_count = 0;
             do
             {
+                int r = RNum();
 
                 while (Console.KeyAvailable == false)
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine(r);
                     Thread.Sleep(1000);
-                    i++;
                 }
 
-
+r = RNum();
                 cki = Console.ReadKey(true);
                 if (cki.Key == ConsoleKey.Spacebar)
                 {
-                    user_input += i;
-                    off_count++;
-                    Console.WriteLine("The user took " + (i - 1));
+                    user_input += r;
+                    //off_count++;
+                    Console.WriteLine("The user took " + r);
+                    
                 }
             } while (cki.Key != ConsoleKey.Escape);
-            Console.WriteLine("Total is: " + (user_input - off_count) + "\nPress Enter to exit");
+            Console.WriteLine("Total is: " + user_input + "\nPress Enter to exit");
             Console.ReadLine();
+        }
+
+        private static int RNum()
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            var byteArray = new byte[4];
+            provider.GetBytes(byteArray);
+
+            //convert 4 bytes to an integer
+            var randomInteger = BitConverter.ToUInt32(byteArray, 0);
+
+            return (int)randomInteger;
         }
     }
 }
